d4d6c47efb518e615ccbfe57ad6fd59f1d0b71da
Amal <146487055+amal554-arch@users.noreply.github.com>
2026-03-31 19:56:22 +0530

    Add a button to create a new grid
    user can enter the number of squares per side.
    new grid will be generated in the same total space as before.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const createGrids = () => {
-    const container = document.querySelector('div');
-    for(let i = 0; i < 16; i++) {
+const createGrids = (squares_per_side = 16) => {
+    const container = document.querySelector('#container');
+    for(let i = 0; i < squares_per_side; i++) {
         const row = document.createElement('div');
         row.classList.add('row')
-        for(let j = 0; j < 16; j++) {
+        for(let j = 0; j < squares_per_side; j++) {
             const grid = document.createElement('div');
             grid.classList.add('grid');
             row.appendChild(grid);
@@ -15,11 +15,19 @@ const createGrids = () => {
             e.target.classList.add('hover-effect');
         }
     });
-    container.addEventListener('mouseout', (e) => {
-        if (!(e.target.id === 'container')) {
-            e.target.classList.remove('hover-effect');
-        }
-    });
 }
+
+document.querySelector("header button").addEventListener('click', () => {
+    const squares_per_side = +prompt("Enter number of squares per side", "16");
+    if (squares_per_side <= 100){
+        const container = document.querySelector('#container');
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+        createGrids(squares_per_side);
+    } else {
+        alert("Too much squares!");
+    }
+})
 
 createGrids();
